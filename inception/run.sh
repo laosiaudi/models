e3996bdb67ea9b3b476523d79acec2ce89bc954a
xiangguang zheng <xiangguz@s-2.tensorflow.cmu849.emulab.net>
2016-12-10 18:13:44 -0700

    add run_metadata

--- inception/run.sh
+++ inception/run.sh
@@ -1,5 +1,7 @@
 sudo bazel-bin/inception/imagenet_distributed_train \
---job_name='ps' \
---task_id=0 \
---ps_hosts='m.tensorflow.emulab.net:2222' \
---worker_hosts='s-1.tensorflow.emulab.net:2222,s-2.tensorflow.emulab.net:2222'
+--batch_size=32 \
+--data_dir=$HOME/imagenet-data \
+--job_name='worker' \
+--task_id=1 \
+--ps_hosts='m.tensorflow.cmu849.emulab.net:2222' \
+--worker_hosts='s-1.tensorflow.cmu849.emulab.net:2222,s-2.tensorflow.cmu849.emulab.net:2222'
