8831b9c2c76198d020ccd15f6b6572ffb358c0c6
xiangguang zheng <xiangguz@s-2.tensorflow.cmu849.emulab.net>
2016-12-10 18:15:05 -0700

    add run_metadata

--- inception/inception/inception_train.py
+++ inception/inception/inception_train.py
@@ -331,11 +331,11 @@ def train(dataset):
         FLAGS.train_dir,
         graph_def=sess.graph.as_graph_def(add_shapes=True))
 
-    run_metadata = tf.RunMetadata()
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
       if step % 10 == 0:
         print 'step is multiple of 10!!!'
+    	run_metadata = tf.RunMetadata()
       	run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
       	_, loss_value = sess.run([train_op, loss], options=run_options, run_metadata=run_metadata)
       else:
