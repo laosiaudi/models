e48851446dbee04fc89b2329b87dcf047c30717c
xiangguang zheng <xiangguz@s-1.tff.cmu849.emulab.net>
2016-12-13 00:29:05 -0700

    new run.sh

--- inception/run.sh
+++ inception/run.sh
@@ -1,7 +1,6 @@
+sudo kill -9 $(ps aux | grep 'bazel' | awk '{print $2}')
 sudo bazel-bin/inception/imagenet_distributed_train \
---batch_size=32 \
---data_dir=$HOME/imagenet-data \
---job_name='worker' \
---task_id=1 \
---ps_hosts='m.tensorflow.cmu849.emulab.net:2222' \
---worker_hosts='s-1.tensorflow.cmu849.emulab.net:2222,s-2.tensorflow.cmu849.emulab.net:2222, s-1.tff.cmu849.emulab.net:2222, s-2.tff.cmu849.emulab.net:2222, s-3.tff.cmu849.emulab.net:2222'
+--job_name='ps' \
+--task_id=0 \
+--ps_hosts='s-1.tff.cmu849.emulab.net:2222' \
+--worker_hosts='s-2.tff.cmu849.emulab.net:2222, s-3.tff.cmu849.emulab.net:2222,s-4.tff.cmu849.emulab.net:2222, s-5.tff.cmu849.emulab.net:2222'''
