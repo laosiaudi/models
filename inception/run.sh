4c70cbc3e589c9f91ec50f31c8118c0df82593bc
xiangguang zheng <xiangguz@s-2.tff.cmu849.emulab.net>
2016-12-13 02:25:40 -0700

    space sucks

--- inception/run.sh
+++ inception/run.sh
@@ -1,6 +1,10 @@
+sudo rm /tmp/*.log
+sudo rm -rf /tmp/imagenet_train
 sudo kill -9 $(ps aux | grep 'bazel' | awk '{print $2}')
 sudo bazel-bin/inception/imagenet_distributed_train \
---job_name='ps' \
+--batch_size=32 \
+--data_dir=$HOME/imagenet-data \
+--job_name='worker' \
 --task_id=0 \
 --ps_hosts='s-1.tff.cmu849.emulab.net:2222' \
---worker_hosts='s-2.tff.cmu849.emulab.net:2222, s-3.tff.cmu849.emulab.net:2222,s-4.tff.cmu849.emulab.net:2222, s-5.tff.cmu849.emulab.net:2222'''
+--worker_hosts='s-2.tff.cmu849.emulab.net:2222,s-3.tff.cmu849.emulab.net:2222,s-4.tff.cmu849.emulab.net:2222,s-5.tff.cmu849.emulab.net:2222,s-6.tff.cmu849.emulab.net:2222,s-7.tff.cmu849.emulab.net:2222'
